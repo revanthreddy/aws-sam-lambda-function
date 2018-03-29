dcd4202a127e2dda16f2f369a1341e54da25b6d8
revanthreddy <revanth.reddy@gmail.com>
2018-03-29 13:17:32 -0500

    Update cfn_run.sh

--- cfn_run.sh
+++ cfn_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #bucket is where SAM uploads the code (zipped) and upload
-bucket_name=python-s3-cleanup
+bucket_name=<bucket_name_goes_here>
 stack_name=sam-lambda-stack
 
 aws cloudformation package --template-file template.yaml --output-template-file serverless-output.yaml --s3-bucket $bucket_name
